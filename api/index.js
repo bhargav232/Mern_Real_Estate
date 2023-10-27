078125409277202571b489050ad82bbbedd86aa3
bhargav232 <bhargavmanavadariya1998@gmail.com>
2023-10-26 21:47:25 -0400

    give sucess message when user data store to db

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config();
 import userrouter from "./Routes/User.route.js"
+import signuprouter from "./Routes/auth.route.js";
 
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -18,4 +19,7 @@ app.listen(3000, ()=>{
     console.log("port is running on 3000!")
 })
 
+app.use(express.json());
+
 app.use("/api/user", userrouter)
+app.use("/api/auth", signuprouter )
