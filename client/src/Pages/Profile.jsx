c31718209531a34ccd15d5fe96f02f0ce7eb01e5
bhargav232 <bhargavmanavadariya1998@gmail.com>
2023-11-19 16:36:42 -0500

    set firebase rules for image upload

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,13 +1,25 @@
 import {useSelector} from "react-redux"
+import { useRef, useState } from "react"
 export default function Profile() {
 const{currentUser} = useSelector(state => state.user)
+const fileRef = useRef(null)
+const[file, setFile] = useState(undefined)
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
         Profile
       </h1>
+      <input onChange ={(e)=>setFile(e.target.files[0])} 
+      type = "file" 
+      ref={fileRef } 
+      hidden 
+      accept="images/*" 
+      />
+      {console.log(file)}
+      
       <form className="flex flex-col gap-4">
-        <img src = {currentUser.avatar} alt = "profile" className= "self-center h-22 w-22 rounded-full object-cover cursor-pointer"/>
+        <img src = {currentUser.avatar} alt = "profile" onClick ={()=>fileRef.current.click()}className= "self-center h-22 w-22 rounded-full object-cover cursor-pointer"/>
         <input type="text" placeholder="username"  id="username" className="p-3 border rounded-lg">
         </input>
         <input type="text" placeholder="email" id="email" className="p-3 border rounded-lg">
@@ -31,3 +43,17 @@ const{currentUser} = useSelector(state => state.user)
     </div>
   )
 }
+
+
+
+// Fire-base image upload rules
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read;
+//       allow write : if
+//       request.resource.size < 2*1024*1024 && 
+//       request.resource.contentType.matches("image/.*")
+//     }
+//   }
+// }
